4654f1e4ad57021863fabcca66dfaae82d53c113
KEVIN MOMBO <Kmombondinga@gmail.com>
2022-06-11 13:58:40 +0200

    Adding my signature comment in the Address class.

--- src/main/java/za/ac/cput/school_management_grp33/domain/location/Address.java
+++ src/main/java/za/ac/cput/school_management_grp33/domain/location/Address.java
@@ -1,4 +1,8 @@
 package za.ac.cput.school_management_grp33.domain.location;
-
+/*
+Address.Java;
+Author: Kevin Lionel Mombo Ndinga(218180500)
+Date: 11 of June 2022;
+ */
 public class Address {
 }
