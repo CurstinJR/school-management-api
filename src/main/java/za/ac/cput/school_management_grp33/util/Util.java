ce9f69dd338767e25ff18c90fc717125bf97037a
KEVIN MOMBO <Kmombondinga@gmail.com>
2022-06-11 13:19:57 +0200

    Import the lang3.StringUtils in the pom file, and setting up the helper class

--- src/main/java/za/ac/cput/school_management_grp33/util/Util.java
+++ src/main/java/za/ac/cput/school_management_grp33/util/Util.java
@@ -1,9 +1,18 @@
 package za.ac.cput.school_management_grp33.util;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Utility Class, also known as Helper class.
  * It contains a bunch of related methods,
  * so they can be reused across the application.
  */
 public final class Util {
+    public static boolean isEmptyOrNull(String str) {
+        return StringUtils.isEmpty(str);
+
+    }
+    public static String setEmptyIfNull(String str) {
+
+    }
 }
