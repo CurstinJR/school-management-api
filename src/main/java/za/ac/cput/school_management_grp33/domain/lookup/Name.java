8dcd1327376926da34f37a36fa0fc48eee23f819
KEVIN MOMBO <Kmombondinga@gmail.com>
2022-06-11 10:31:29 +0200

    Creation of classes and interfaces

--- src/main/java/za/ac/cput/school_management_grp33/domain/lookup/Name.java
+++ src/main/java/za/ac/cput/school_management_grp33/domain/lookup/Name.java
@@ -1,4 +1,9 @@
 package za.ac.cput.school_management_grp33.domain.lookup;
-
+/*
+Name.java
+Author: Kevin Lionel Mombo Ndinga(218180500)
+Date: 11 of June 2022;
+ */
 public class Name {
+    private String firstName,middleName,lastName;
 }
