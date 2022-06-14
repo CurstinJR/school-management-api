faceae6b327f7b8c6095cf1e2be6c706bf870c61
KEVIN MOMBO <Kmombondinga@gmail.com>
2022-06-14 21:51:25 +0200

    setting up the AddressFactory

--- src/main/java/za/ac/cput/school_management_grp33/factory/lookup/AddressFactory.java
+++ src/main/java/za/ac/cput/school_management_grp33/factory/lookup/AddressFactory.java
@@ -1,4 +1,23 @@
 package za.ac.cput.school_management_grp33.factory.lookup;
 
+import za.ac.cput.school_management_grp33.domain.location.City;
+import za.ac.cput.school_management_grp33.domain.lookup.Address;
+import za.ac.cput.school_management_grp33.util.Util;
+
 public class AddressFactory {
+    public static Address build(String unitNumber, String complexName, String complexNumber, String streetNumber,
+                                String streetName, int postalCode, City city) {
+
+        Util.checkStringParam("unitNumber",unitNumber);
+        Util.checkStringParam("complexName",complexName);
+        Util.checkStringParam("complexNumber",complexNumber);
+        Util.checkStringParam("streetNumber",streetNumber);
+        Util.checkStringParam("streetName",streetName);
+        Util.checkRangeNum(postalCode);
+
+         return Address.builder().unitNumber(unitNumber).complexName(complexName).complexNumber(complexNumber).streetNumber(streetNumber).streetName(streetName )
+                 .city(city).build();
+    }
+
 }
+
