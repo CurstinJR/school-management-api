3801f464d884061f1a901646435dbc904b9bde92
KEVIN MOMBO <Kmombondinga@gmail.com>
2022-06-14 10:34:29 +0200

    Adding the annotation in the address class

--- src/main/java/za/ac/cput/school_management_grp33/domain/lookup/Address.java
+++ src/main/java/za/ac/cput/school_management_grp33/domain/lookup/Address.java
@@ -1,5 +1,9 @@
 package za.ac.cput.school_management_grp33.domain.lookup;
 
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 import za.ac.cput.school_management_grp33.domain.location.City;
 
 /*
@@ -7,6 +11,10 @@ Address.java
 Author:Kevin Lionel Mombo Ndinga(218180500)
 Date: 14 of June 2022;
  */
+@Builder
+@Getter
+@ToString
+@EqualsAndHashCode
 public class Address {
     private String unitNumber;
     private String complexName;
