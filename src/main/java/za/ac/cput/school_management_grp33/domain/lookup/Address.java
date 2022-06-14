6b2766625e7d75d7e0334b748eec7c235225a815
KEVIN MOMBO <Kmombondinga@gmail.com>
2022-06-14 10:14:22 +0200

    creating the address class

--- src/main/java/za/ac/cput/school_management_grp33/domain/lookup/Address.java
+++ src/main/java/za/ac/cput/school_management_grp33/domain/lookup/Address.java
@@ -1,8 +1,11 @@
 package za.ac.cput.school_management_grp33.domain.lookup;
+
+import za.ac.cput.school_management_grp33.domain.location.City;
+
 /*
 Address.java
 Author:Kevin Lionel Mombo Ndinga(218180500)
-Date: 12 of June 2022;
+Date: 14 of June 2022;
  */
 public class Address {
     private String unitNumber;
@@ -10,5 +13,5 @@ public class Address {
     private String streetNumber;
     private String streetName;
     private int postalCode;
-   // private City city;
+   private City city;
 }
