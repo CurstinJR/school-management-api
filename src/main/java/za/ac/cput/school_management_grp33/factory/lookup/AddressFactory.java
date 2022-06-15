7c20fcc739eae0d444bf24f6fe128e1f272daafe
KEVIN MOMBO <Kmombondinga@gmail.com>
2022-06-15 10:40:04 +0200

    modifying the return statement

--- src/main/java/za/ac/cput/school_management_grp33/factory/lookup/AddressFactory.java
+++ src/main/java/za/ac/cput/school_management_grp33/factory/lookup/AddressFactory.java
@@ -3,7 +3,10 @@ package za.ac.cput.school_management_grp33.factory.lookup;
 import za.ac.cput.school_management_grp33.domain.location.City;
 import za.ac.cput.school_management_grp33.domain.lookup.Address;
 import za.ac.cput.school_management_grp33.util.Util;
-
+/*
+Author:Kevin Lionel Mombo Ndinga (218180500)
+AddressTest.java
+ */
 public class AddressFactory {
     public static Address build(String unitNumber, String complexName, String complexNumber, String streetNumber,
                                 String streetName, int postalCode, City city) {
@@ -15,7 +18,10 @@ public class AddressFactory {
         Util.checkStringParam("streetName",streetName);
         Util.checkRangeNum(postalCode);
 
-         return Address.builder().unitNumber(unitNumber).complexName(complexName).complexNumber(complexNumber).streetNumber(streetNumber).streetName(streetName )
+         return Address.builder().unitNumber(unitNumber).complexName(complexName)
+                 .complexNumber(complexNumber)
+                 .streetNumber(streetNumber)
+                 .streetName(streetName )
                  .city(city).build();
     }
 
