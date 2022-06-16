0a2bbe9fb0272da736f060ab1bd5735f690f0c9d
Tarren-Marc Adams <you@example.com>
2022-06-16 13:05:04 +0200

    Created CountryRepository and CountryService

--- src/main/java/za/ac/cput/school_management_grp33/repository/location/CountryRepository.java
+++ src/main/java/za/ac/cput/school_management_grp33/repository/location/CountryRepository.java
@@ -1,4 +1,13 @@
 package za.ac.cput.school_management_grp33.repository.location;
 
-public interface CountryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.school_management_grp33.domain.location.Country;
+
+/*
+CountryRepository.java
+Author: Tarren-Marc Adams - 214041794
+Date: 16 March 2022
+ */
+public interface CountryRepository extends JpaRepository<Country, String> {
+
 }
--- src/main/java/za/ac/cput/school_management_grp33/service/location/CountryService.java
+++ src/main/java/za/ac/cput/school_management_grp33/service/location/CountryService.java
@@ -1,4 +1,13 @@
 package za.ac.cput.school_management_grp33.service.location;
 
-public interface CountryService {
+import za.ac.cput.school_management_grp33.domain.location.Country;
+import za.ac.cput.school_management_grp33.service.IService;
+
+/*
+CountryService.java
+Author: Tarren-Marc Adams - 214041794
+Date: 16 March 2022
+ */
+public interface CountryService extends IService <Country, String> {
+
 }
