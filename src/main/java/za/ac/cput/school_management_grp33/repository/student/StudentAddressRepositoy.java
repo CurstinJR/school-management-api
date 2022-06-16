2be76f9c65d1167b8bb7538c40c86c5c2382b6d8
KEVIN MOMBO <Kmombondinga@gmail.com>
2022-06-16 16:12:46 +0200

    Creating the classes and interfaces for StudentAddress

---
+++ src/main/java/za/ac/cput/school_management_grp33/repository/student/StudentAddressRepositoy.java
@@ -0,0 +1,4 @@
+package za.ac.cput.school_management_grp33.repository.student;
+
+public interface StudentAddressRepositoy {
+}
---
+++ src/main/java/za/ac/cput/school_management_grp33/service/student/impl/StudentAddressServiceImpl.java
@@ -0,0 +1,4 @@
+package za.ac.cput.school_management_grp33.service.student.impl;
+
+public class StudentAddressServiceImpl {
+}
