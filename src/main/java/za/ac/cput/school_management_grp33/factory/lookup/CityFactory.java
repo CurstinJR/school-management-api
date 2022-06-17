06cd27f62f07aa92a3f424273914ae20db2b7967
Chantal Niyonzima <80648854+Niyonzima10@users.noreply.github.com>
2022-06-17 10:12:12 +0200

    While working, getting the latest.

---
+++ src/main/java/za/ac/cput/school_management_grp33/factory/lookup/CityFactory.java
@@ -0,0 +1,13 @@
+package za.ac.cput.school_management_grp33.factory.lookup;
+
+import za.ac.cput.school_management_grp33.domain.location.City;
+import za.ac.cput.school_management_grp33.domain.location.Country;
+
+public class CityFactory {
+    public static City getCity(String id, String name, Country country){
+            return City.builder()
+                    .id(id)
+                    .name(name)
+                    .country(country).build();
+    }
+}
