e842294260f8923768329bf046db630ad94a4fc7
Tarren-Marc Adams <you@example.com>
2022-06-17 22:54:45 +0200

    Created CountryServiceImpl, Service interface, Repository, and Controller

--- src/main/java/za/ac/cput/school_management_grp33/repository/location/CountryRepository.java
+++ src/main/java/za/ac/cput/school_management_grp33/repository/location/CountryRepository.java
@@ -1,4 +1,8 @@
 package za.ac.cput.school_management_grp33.repository.location;
 
-public interface CountryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.school_management_grp33.domain.location.Country;
+
+public interface CountryRepository extends JpaRepository<Country,String> {
+
 }
