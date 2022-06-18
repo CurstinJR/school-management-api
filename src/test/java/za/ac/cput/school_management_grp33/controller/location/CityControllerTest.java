2f95013040716f9816d9b7da423192ef6bfb3923
Tarren-Marc Adams <you@example.com>
2022-06-18 19:22:01 +0200

    Question 7 find cities by country Id updates

--- src/test/java/za/ac/cput/school_management_grp33/controller/location/CityControllerTest.java
+++ src/test/java/za/ac/cput/school_management_grp33/controller/location/CityControllerTest.java
@@ -7,10 +7,7 @@
 package za.ac.cput.school_management_grp33.controller.location;
 
 import org.junit.FixMethodOrder;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,6 +19,9 @@ import za.ac.cput.school_management_grp33.domain.location.Country;
 import za.ac.cput.school_management_grp33.factory.location.CityFactory;
 import za.ac.cput.school_management_grp33.factory.location.CountryFactory;
 import za.ac.cput.school_management_grp33.repository.location.CountryRepository;
+
+import java.util.Arrays;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -47,6 +47,7 @@ class CityControllerTest {
     }
 
     @Test
+    @Order(1)
     void a_createUpdate() {
         this.countryRepository.save(country);
         ResponseEntity<City> response = restTemplate.postForEntity(baseUrl, cityObject, City.class);
@@ -54,25 +55,30 @@ class CityControllerTest {
     }
 
     @Test
+    @Order(2)
     void b_read() {
         ResponseEntity<City> response = restTemplate.getForEntity(baseUrl + cityObject.getId(), City.class);
         assertNotNull(response.getBody());
     }
 
     @Test
+    @Order(3)
     void readAll() {
         ResponseEntity<String> response = restTemplate.getForEntity(baseUrl + cityObject.getId(), String.class);
         assertNotNull(response.getBody());
     }
 
     @Test
+    @Order(4)
     void getCitiesByCountryId(){
-
-
+        ResponseEntity<City[]> response = restTemplate.getForEntity(baseUrl, City[].class);
+        System.out.println(Arrays.asList(Objects.requireNonNull(response.getBody())));
+        assertNotNull(response.getBody());
     }
 
 
     @Test
+    @Order(5)
     void delete() {
         restTemplate.delete(baseUrl + cityObject.getId(), String.class);
         ResponseEntity<City> response = restTemplate.getForEntity(baseUrl + cityObject.getId(), City.class);
