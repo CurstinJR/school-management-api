9b98a3120098faae211b33f4bb211bab7a190533
Chantal Niyonzima <80648854+Niyonzima10@users.noreply.github.com>
2022-06-18 19:23:36 +0200

    I can now get All the names of employees with cityId Q6. (#48)
    * While working, getting the latest.
    
    * Done with my part, but I am still waiting for Address and EmployeeAddress.
    ! I have implemented CountryRepository so that i can be able to create a City, since City depends on country.
    
    * While working, getting the latest.
    
    * I can now get All the Names of employees in a city with a cityId.

--- src/test/java/za/ac/cput/school_management_grp33/controller/location/CityControllerTest.java
+++ src/test/java/za/ac/cput/school_management_grp33/controller/location/CityControllerTest.java
@@ -17,11 +17,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
+import za.ac.cput.school_management_grp33.domain.employee.Employee;
+import za.ac.cput.school_management_grp33.domain.employee.EmployeeAddress;
 import za.ac.cput.school_management_grp33.domain.location.City;
 import za.ac.cput.school_management_grp33.domain.location.Country;
+import za.ac.cput.school_management_grp33.domain.lookup.Address;
+import za.ac.cput.school_management_grp33.domain.lookup.Name;
 import za.ac.cput.school_management_grp33.factory.location.CityFactory;
 import za.ac.cput.school_management_grp33.factory.location.CountryFactory;
+import za.ac.cput.school_management_grp33.repository.employee.EmployeeAddressRepository;
+import za.ac.cput.school_management_grp33.repository.employee.EmployeeRepository;
 import za.ac.cput.school_management_grp33.repository.location.CountryRepository;
+import za.ac.cput.school_management_grp33.service.location.impl.CityServiceImpl;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -37,9 +44,15 @@ class CityControllerTest {
     private int PORT;
     private String baseUrl;
     @Autowired
+    private CityServiceImpl service;
+    @Autowired
     private TestRestTemplate restTemplate;
     @Autowired
     private CountryRepository countryRepository;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private EmployeeAddressRepository employeeAddressRepository;
 
     @BeforeEach
     void setUp() {
@@ -60,15 +73,38 @@ class CityControllerTest {
     }
 
     @Test
-    void readAll() {
+    void c_readAll() {
         ResponseEntity<String> response = restTemplate.getForEntity(baseUrl + cityObject.getId(), String.class);
         assertNotNull(response.getBody());
     }
 
     @Test
-    void delete() {
+    void d_delete() {
         restTemplate.delete(baseUrl + cityObject.getId(), String.class);
         ResponseEntity<City> response = restTemplate.getForEntity(baseUrl + cityObject.getId(), City.class);
         assertNull(response.getBody().getId());
     }
+    @Test
+    void e_findEmployeesByCityId(){
+        Country country = Country.builder().name("Tanzania").id("1234").build();
+        Country country1 = countryRepository.save(country);
+        System.out.println(country1);
+        City city = CityFactory.getCity("1230", "Brazaville", country);
+        City city1 = service.save(city);
+        System.out.println("City created sucessfully: "+city1);
+
+        Name name = Name.builder().firstName("Max").lastName("Well").middleName("Robert").build();
+        Employee employee = Employee.builder().name(name).email("max@well.com").staffId("0001").build();
+        Employee employeResult = employeeRepository.save(employee);
+        System.out.println("employee :"+employeResult);
+
+        Address address = Address.builder().city(city).complexName("Butros").streetName("Reebieck").streetNumber("20").postalCode(6739).build();
+        EmployeeAddress employeeAddress = EmployeeAddress.builder().address(address).staffId("0001").build();
+        EmployeeAddress employeeAddress1 = employeeAddressRepository.save(employeeAddress);
+        System.out.println("employeeAddress :"+employeeAddress1);
+
+        ResponseEntity<String> response = restTemplate.getForEntity(baseUrl+"employees/" + city1.getId(), String.class);
+        assertNotNull(response.getBody());
+    }
+
 }
